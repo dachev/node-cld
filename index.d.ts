92707ced4a06abe6ab3fb89e783808f342df2a32
Blagovest Dachev <blago@dachev.com>
2022-06-27 07:20:24 +0700

    Adds Typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+interface Language {
+  readonly name: string;
+  readonly code: string;
+  readonly percent: number;
+  readonly score: number;
+}
+interface Chunk {
+  readonly name: string;
+  readonly code: string;
+  readonly offset: number;
+  readonly bytes: number;
+}
+interface Options {
+  readonly isHTML: false;
+  readonly languageHint: string;
+  readonly encodingHint: string;
+  readonly tldHint: string;
+  readonly httpHint: string;
+}
+interface DetectLanguage {
+  readonly reliable: boolean;
+  readonly textBytes: number;
+  readonly languages: Language[];
+  readonly chunks: Chunk[];
+}
+export declare module 'cld' {
+  declare function detect(text: string, options: Options, callback: (err: string, result: DetectLanguage) => {});
+  declare function detect(text: string, callback: (err: string, result: DetectLanguage) => {});
+}
